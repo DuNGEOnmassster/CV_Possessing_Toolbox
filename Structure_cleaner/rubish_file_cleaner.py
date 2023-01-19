5f2aa183abe801d94e8bde970d3cd9a6b25d555a
NormanZ <1275133384@qq.com>
2023-01-19 22:18:33 +0800

    update and successfully test rubish file cleaner

--- Structure_cleaner/rubish_file_cleaner.py
+++ Structure_cleaner/rubish_file_cleaner.py
@@ -1,15 +1,28 @@
+'''
+Rewrite the path, adjust the prefixes and suffixes as you wish
+'''
+
 import os
 
 image_path = "./test_dataset"
+clean_by_prefixes = True
+clean_by_suffixes = False
 
+prefixes = "._"
+suffixes = ".png"
 
 def del_files(path):
     for root , dirs, files in os.walk(path):
         for name in files:
-            if name.startswith("._"):
-                os.remove(os.path.join(root,name))         
-                print ("Delete File: " + os.path.join(root, name))
+            if clean_by_prefixes:
+                if name.startswith(prefixes):
+                    os.remove(os.path.join(root,name))         
+                    print ("Delete File: " + os.path.join(root, name))
+
+            if clean_by_prefixes:
+                if name.endswith(suffixes):
+                    os.remove(os.path.join(root,name))         
+                    print ("Delete File: " + os.path.join(root, name))
 
 
 del_files(image_path)
-
