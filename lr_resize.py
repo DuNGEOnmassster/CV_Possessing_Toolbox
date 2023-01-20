4751ece61d89710d7b16f5c65425deb43b41e75b
NormanZ <1275133384@qq.com>
2023-01-20 18:16:42 +0800

    update lr resize

--- lr_resize.py
+++ lr_resize.py
@@ -8,13 +8,25 @@ import cv2
 image_path = "./test_dataset/"
 target_path = "./target_dataset/lr_resize/"
 
+LR_path = "xxxx_LR/"
+HR_path = "xxxx_HR/"
+
+
+def get_HRname_from_LRname(LR_name):
+    return LR_name.replace("LR", "HR")
+
 
 def get_paired_lrsr():
-    for root , dirs, files in os.walk(image_path):
+    for root , dirs, files in os.walk(image_path + LR_path):
         for name in files:
-            print("\n" + name)
-
-
+            if name.endswith(".png"):       
+                print ("Find LR image: " + os.path.join(root, name))
+                HR_name = get_HRname_from_LRname(name)
+                # print(image_path + HR_path + HR_name)
+                lr_img = Image.open(root + name)
+                hr_img = Image.open(image_path + HR_path + HR_name)
+                print(f"lr size = {lr_img.size}, hr size = {hr_img.size}")
+            
 
 
 
