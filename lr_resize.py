a1f08c66ffe25dcc373ff42500f101cdff25f2d0
NormanZ <1275133384@qq.com>
2023-01-20 19:15:33 +0800

    upload demo for lr resize

--- lr_resize.py
+++ lr_resize.py
@@ -16,19 +16,30 @@ def get_HRname_from_LRname(LR_name):
     return LR_name.replace("LR", "HR")
 
 
-def get_paired_lrsr():
+def get_paired_lrhr():
+    paired_lrhr = []
     for root , dirs, files in os.walk(image_path + LR_path):
         for name in files:
             if name.endswith(".png"):       
                 print ("Find LR image: " + os.path.join(root, name))
                 HR_name = get_HRname_from_LRname(name)
-                # print(image_path + HR_path + HR_name)
                 lr_img = Image.open(root + name)
                 hr_img = Image.open(image_path + HR_path + HR_name)
-                print(f"lr size = {lr_img.size}, hr size = {hr_img.size}")
                 lr_img = lr_img.resize((hr_img.size[0], hr_img.size[1]), Image.Resampling.BICUBIC)
-                print("After BICUBIC:")
-                print(f"lr size = {lr_img.size}, hr size = {hr_img.size}")
+                # print("After BICUBIC:")
+                # print(f"lr size = {lr_img.size}, hr size = {hr_img.size}")
+                paired_lrhr.append([lr_img, hr_img])
+
+    return paired_lrhr
+    
+
+def save_paired(paired_lrhr):
+    index = 1
+    for item in paired_lrhr:
+        item[0].save(target_path + str(index) + '_LR.png', 'PNG')
+        item[1].save(target_path + str(index) + '_HR.png', 'PNG')
+        index += 1
 
 if __name__ == "__main__":
-    get_paired_lrsr()
+    paired_lrhr = get_paired_lrhr()
+    save_paired(paired_lrhr)
