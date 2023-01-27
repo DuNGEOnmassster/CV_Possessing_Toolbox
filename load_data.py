1e8f09d88224bf9ce0e50bb95869710dd62997fc
NormanZ <1275133384@qq.com>
2023-01-27 17:09:48 +0800

    update load data

--- load_data.py
+++ load_data.py
@@ -1,83 +1,33 @@
-import os
-import albumentations
-import numpy as np
-import torch.nn as nn
-from PIL import Image
-from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Image data loader")
-
-    parser.add_argument('--dataset_path', type=str, default='/Users/normanz/Desktop/Github/CV_Possessing_Toolbox/test_dataset/xxxx_HR',
-                        help='Declare the dataset path')
-    parser.add_argument('--device', type=str, default="cuda",
-                        help='Declare whether to use cuda')
-    parser.add_argument('--batch-size', type=int, default=2,
-                        help='Declare batch size for training')
+    parser = argparse.ArgumentParser(description="dataset params")
 
     return parser.parse_args()
 
-class ImagePaths(Dataset):
-    def __init__(self, path, size=None):
-        self.size = size
 
-        self.images = [os.path.join(path, file) for file in os.listdir(path)]
-        self._length = len(self.images)
+def get_dataloader(batch_size, args):
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+    # Get Dataset
+    train_dataset = torchvision.datasets.ImageFolder(args.data_dir, train=True, transform=data_transform["train"], download=False)
+    test_dataset = torchvision.datasets.ImageFolder(args.data_dir, train=False, transform=data_transform["val"], download=False)
+    print('length of trainning dataset: {}'.format(len(train_dataset)))
+    print('length of testing dataset: {}'.format(len(test_dataset)))
+    # Get DataLoader
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    return train_dataloader,test_dataloader
 
-        self.rescaler = albumentations.SmallestMaxSize(max_size=self.size)
-        self.cropper = albumentations.CenterCrop(height=self.size, width=self.size)
-        self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
-
-    def __len__(self):
-        return self._length
-
-    def preprocess_image(self, image_path):
-        image = Image.open(image_path)
-        if not image.mode == "RGB":
-            image = image.convert("RGB")
-        image = np.array(image).astype(np.uint8)
-        image = self.preprocessor(image=image)["image"]
-        image = (image / 127.5 - 1.0).astype(np.float32)
-        image = image.transpose(2, 0, 1)
-        return image
-
-    def __getitem__(self, i):
-        example = self.preprocess_image(self.images[i])
-        return example
-
-
-def load_data(args):
-    train_data = ImagePaths(args.dataset_path, size=256)
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False)
-    return train_loader
-
-
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
-
-
-def plot_images(images):
-    x = images["input"]
-    reconstruction = images["rec"]
-    half_sample = images["half_sample"]
-    full_sample = images["full_sample"]
-
-    fig, axarr = plt.subplots(1, 4)
-    axarr[0].imshow(x.cpu().detach().numpy()[0].transpose(1, 2, 0))
-    axarr[1].imshow(reconstruction.cpu().detach().numpy()[0].transpose(1, 2, 0))
-    axarr[2].imshow(half_sample.cpu().detach().numpy()[0].transpose(1, 2, 0))
-    axarr[3].imshow(full_sample.cpu().detach().numpy()[0].transpose(1, 2, 0))
-    plt.show()
-
-if __name__ == "__main__":
-    args = parse_args()
-    train_dataset = load_data(args)
-    print(train_dataset.__len__())
