41dab760df6e52ced8702b298fe416cc6ad7fed1
NormanZ <1275133384@qq.com>
2023-04-17 17:53:58 +0800

    update

--- classify_dataset.py
+++ classify_dataset.py
@@ -14,11 +14,20 @@ def save_lrhr(lr_imgs, hr_imgs, root):
     lr_path = os.path.join(root, "LR")
     hr_path = os.path.join(root, "HR")
     print(lr_path, hr_path)
+    for item in range(len(lr_imgs)):
+        lr_save_path = os.path.join(lr_path,lr_imgs[item][1])
+        hr_save_path = os.path.join(hr_path,hr_imgs[item][1])
+        if not os.path.exists(lr_save_path):
+            os.mkdir(lr_save_path)
+        if not os.path.exists(hr_save_path):
+            os.mkdir(hr_save_path)
+
+        # print(f"lr: {os.path.join(lr_path,lr_imgs[item][1])}, hr: {os.path.join(hr_path,hr_imgs[item][1])}")
+        lr_imgs[item][0].save(lr_save_path, 'PNG')
+        hr_imgs[item][0].save(hr_save_path, 'PNG')
 
 
-
-
-def get_paired_lrhr(dataset_path):
+def get_saved_lrhr(dataset_path):
     paired_lrhr = []
     for root , dirs, files in os.walk(dataset_path):
         # lr_path = os.path.join(root, "LR")
@@ -46,13 +55,11 @@ def get_paired_lrhr(dataset_path):
         if save_flag:
             save_lrhr(lr_imgs, hr_imgs, root)
 
-    return paired_lrhr
 
 
 if __name__ == "__main__":
     dataset_path = "./target_dataset/"
     # LR_path = "xxxx_LR/"
     # HR_path = "xxxx_HR/"
-
-    paired_lrhr = get_paired_lrhr(dataset_path)
+    get_saved_lrhr(dataset_path)
     # print(paired_lrhr)
