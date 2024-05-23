d1b219f220c2c66eb72dcae11400ab85098c68f5
DuNGEOnmassster <normanzheng6606@gmail.com>
2024-05-23 20:12:34 +0800

    update clean rubish dir function

--- Structure_cleaner/rubish_file_cleaner.py
+++ Structure_cleaner/rubish_file_cleaner.py
@@ -11,6 +11,9 @@ clean_by_suffixes = False
 prefixes = ".DS_S"
 suffixes = ".png"
 
+dir_path = "../../output"
+dir_names = "point_cloud"
+
 def del_files(path):
     for root , dirs, files in os.walk(path):
         for name in files:
@@ -24,5 +27,21 @@ def del_files(path):
                     os.remove(os.path.join(root,name))         
                     print ("Delete File: " + os.path.join(root, name))
 
+def del_dirs(path):
+    for root , dirs, files in os.walk(path):
+        for name in dirs:
+            if dir_names in name:
+                try:
+                    os.rmdir(os.path.join(root,name))
+                    print ("Delete Dir with os.rmdir: " + os.path.join(root, name))
+                except OSError:
+                    os.system("rm -rf " + os.path.join(root,name))
+                    print ("Delete Dir with os.system rm -rf: " + os.path.join(root, name))
+                
 
-del_files(image_path)
+
+if __name__ == '__main__':
+    # del_files(image_path)
+    del_dirs(dir_path)
+
+    # del_files(image_path)
